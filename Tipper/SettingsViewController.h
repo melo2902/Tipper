0b2603bbd4a03ef0dd8025621f674bd6d2b174b5
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-06-22 18:24:04 -0400

    Styling + Settings
    Update styling on front page, framework of settings page

---
+++ Tipper/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  Tipper
+//
+//  Created by mwen on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
+
